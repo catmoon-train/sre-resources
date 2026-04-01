9aa3d43f58908271f5c50d1fc443810d7f847e64
wifi-left <ysy1679682327@outlook.com>
2026-04-01 19:05:26 +0800

    sre-resources

---
+++ src/main/java/io/sre/resource_lib/SREResource.java
@@ -0,0 +1,21 @@
+package io.sre.resource_lib;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SREResource implements ModInitializer {
+	public static final String MOD_ID = "sre_resource";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
